51770bd99d094fc7b5872eca883f11bf97bc8d8d
dustinger <dustinger@gmail.com>
2024-09-07 11:19:35 -0500

    Update portscan.sh
    fixed error

--- portscan.sh
+++ portscan.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-read target -r
-echo text| nmap -O -p- -sV -sC -sU target
-echo Scan complete
+
+# Read the target input
+read -r target
+
+# Run the Nmap scan on the target with the correct options
+nmap -O -p- -sV -sC -sU "$target"
+
+# Print completion message
+echo "Scan complete"
